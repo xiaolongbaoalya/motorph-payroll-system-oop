ff2609e252d6c9d710e48a8dbfa546dc0959c6d4
lrsrivera <lr.srivera@mmdc.mcl.edu.ph>
2026-03-14 23:06:23 +0800

    Delete src directory

--- src/main/java/com/compprog1282025/model/interfaces/Calculable.java
+++
@@ -1,5 +0,0 @@
-package com.compprog1282025.model.interfaces;
-
-public interface Calculable {
-	double calculateNet(double amount);
-}
--- src/main/java/com/compprog1282025/model/user/EffectiveRole.java
+++
@@ -1,9 +0,0 @@
-package com.compprog1282025.model.user;
-
-public enum EffectiveRole {
-    HR,
-    ADMIN,
-    FINANCE,
-    IT,
-    EMPLOYEE
-}
